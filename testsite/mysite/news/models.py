4de4f887df3a934ca65575398ca25b3b54b54ec6
lapssh <lapssh@yandex.ru>
2020-10-23 23:05:29 +0300

    добавлена новая модель для приложения News

--- testsite/mysite/news/models.py
+++ testsite/mysite/news/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 
-
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # дата и время устанавливаются лишь раз
+    updated_at = models.DateTimeField(auto_now=True)  # обновляется при каждом редактировании
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
