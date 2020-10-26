21a56ea4638de7fd1e42485da5baa5122b3a5982
lapssh <lapssh@yandex.ru>
2020-10-27 00:08:56 +0300

    добавлена вьюха по категориям сделано боковое меню для фильтра новостей по категориям.

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+from .models import News, Category
 
-from .models import News
 
 def index(request):
     news = News.objects.all()
-    context =  {
+    categories = Category.objects.all()
+    context = {
         'news': news,
-        'title': 'Список новостей'
+        'title': 'Список новостей',
+        'categories': categories,
     }
-    return render(request, 'news/index.html', context)
+    return render(request, template_name='news/index.html', context=context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, template_name='news/category.html', context={'news': news, 'categories': categories,
+                                                                        'category': category})
