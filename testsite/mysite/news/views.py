a90c227e3517c73fb3971d945605b8496c3f3069
lapssh <lapssh@yandex.ru>
2020-10-27 23:26:51 +0300

    - добавлены имена маршрутов - добавлен пользовательский тег шаблона для категорий - добавлено обратное разрешение адресов через get_absolute_url - реализован функционал просмотра отдельных новостей.

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categories': categories,
     }
     return render(request, template_name='news/index.html', context=context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, template_name='news/category.html', context={'news': news, 'categories': categories,
-                                                                        'category': category})
+    return render(request, template_name='news/category.html', context={'news': news, 'category': category})
+
+def view_news(request, news_id):
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, template_name='news/view_news.html', context={'news_item': news_item})
