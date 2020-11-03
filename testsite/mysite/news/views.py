662c2082bc7bf1705bc99af5017d6766ade26053
lapssh <lapssh@yandex.ru>
2020-11-04 00:45:37 +0300

    - добавлена кастомная валидация - добавлены формы ListView

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,23 +1,50 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
 
 from .models import News, Category
 from .forms import NewsForm
 
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    # extra_context = {'title': 'Главная '}
 
-def index(request):
-    news = News.objects.all()
-    context = {
-        'news': news,
-        'title': 'Список новостей',
-    }
-    return render(request, template_name='news/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
+
+# def index(request):
+#     news = News.objects.all()
+#     context = {
+#         'news': news,
+#         'title': 'Список новостей',
+#     }
+#     return render(request, template_name='news/index.html', context=context)
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-    return render(request, template_name='news/category.html', context={'news': news, 'category': category})
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     return render(request, template_name='news/category.html', context={'news': news, 'category': category})
 
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
 def view_news(request, news_id):
     news_item = get_object_or_404(News, pk=news_id)
@@ -26,7 +53,11 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == 'POST':
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # news = News.objects.create(**form.cleaned_data)
+            news = form.save()
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, template_name='news/add_news.html', context={'form': form})
